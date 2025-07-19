857a78ed7725acb5e6da1876450e5745bddead54
Sanket Vyawahare <srvyawahare18@gmail.com>
2025-07-19 23:30:28 +0530

    Implemented basic project setup with postgres db

---
+++ src/main/java/com/eventiq/monitoring/EventiqMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.eventiq.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventiqMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventiqMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eventiq/monitoring/EventiqMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventiq.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventiqMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
